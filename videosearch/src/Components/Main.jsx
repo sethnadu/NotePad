df650ee1fdfc67acdf203c5449a3dacdef62e2e8
Seth Nadu <seth.nadu@gmail.com>
2020-01-31 17:14:27 -0500

    added gif for welcome screen

--- videosearch/src/Components/Main.jsx
+++ videosearch/src/Components/Main.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import Appbar from './Appbar'
 import ResultCard from './ResultCard'
 import Intro from './Intro'
+import TheaterGif from '../Assets/Images/theater.gif'
 import {Link, Route} from 'react-router-dom'
 import SinglePage from './SinglePage'
 
@@ -15,9 +16,19 @@ function Main({ data, setSingleResult, singleResult}){
     return (
         <>
         <div style={{"margin": "20px", "display": "flex", 'flexFlow': 'row wrap', "justifyContent": "center"}}>
-            {data ? data.map(result => {
+            {data != '' ? data.map(result => {
                 return <ResultCard singleResult={singleResult} setSingleResult = {setSingleResult} key={result.name} result={result} data={data}/>
-            }) : <h2 style={{"marginTop": "150px"}}>Search above!</h2>}
+            }) : (
+            <div style={{'display': 'flex', 'flexDirection': 'column'}}>
+            <img style={{
+                "margin": '20px auto', 
+                'width': '100%', 
+                'border': '2px solid black',
+                'boxShadow': "0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)", 
+                'borderRadius': '4px'}} 
+                src={TheaterGif} alt="old movie theater popcorn dancing" />
+            </div>
+            )}
         </div>
         </>
     )
